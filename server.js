0c36281416e58b462c6e4f5ffdcb646c77518b0f
anil kadam <anilkadam210@gmail.com>
2020-02-17 00:57:45 +0530

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 
+const mockUserData = [
+    {name: "Anil"},
+    {name: "Rohit"}
+];
+
+app.get('/users', (req,res)=>{
+    res.json({
+        success: true,
+        message: 'successfully got user. Nice!',
+        users: mockUserData
+    });
+});
+
 app.listen(8000,() => {
     console.log("server is listening");
 });
