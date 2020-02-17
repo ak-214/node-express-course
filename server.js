9799e989cf14a2305aed3d5ddb9805f8feb93bf4
anil kadam <anilkadam210@gmail.com>
2020-02-17 21:24:32 +0530

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 const mockUserData = [
     {name: "Anil"},
@@ -14,6 +17,27 @@ app.get('/users/:id', (req,res)=>{
         message: 'successfully got user. Nice!',
         user: req.params.id
     });
+});
+
+app.post('/login', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    const mockUserName = "Anil";
+    const mockPassword = "kadam";
+
+    if(username === mockUserName && password === mockPassword) {
+        res.json({
+            success: true,
+            message: 'password and username match',
+            token: 'encrypted token goes here'
+        });
+    } else {
+        res.json({
+            success: true,
+            message: 'password and username do not match'
+        });
+    }
 });
 
 app.listen(8000,() => {
